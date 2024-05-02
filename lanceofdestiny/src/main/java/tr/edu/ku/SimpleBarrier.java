628c5a7b6de08dacf2480507096ee3a1c5b5e8bf
atemizkan20 <atemizkan20@ku.edu.tr>
2024-05-02 22:54:04 +0300

    spells added

--- lanceofdestiny/src/main/java/tr/edu/ku/SimpleBarrier.java
+++ lanceofdestiny/src/main/java/tr/edu/ku/SimpleBarrier.java
@@ -1,10 +1,9 @@
-package tr.edu.ku;
+package tr.edu.ku.Domain;
 
 public class SimpleBarrier extends Barrier {
 
     public SimpleBarrier(int x, int y, int width, int height) {
         super(x, y, width, height);
-
 
     }
 }
