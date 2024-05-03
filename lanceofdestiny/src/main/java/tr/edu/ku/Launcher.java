6c6ffa3efa228dbaa6d742080e17e9184006df17
Efeispir <eispir19ku.edu.tr>
2024-05-03 11:00:57 +0300

    cleaning branch

--- lanceofdestiny/src/main/java/tr/edu/ku/Launcher.java
+++
@@ -1,9 +0,0 @@
-package tr.edu.ku;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        Player player = new Player();
-        MainApplication app = new MainApplication(player);
-    }
-}
